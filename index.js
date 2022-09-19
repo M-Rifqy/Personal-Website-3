5ac96e729ab4a178a7670bdd42bd0a424f98f6f3
Muhammad Rifqy <2ndrifqy@gmail.com>
2022-09-19 21:27:29 +0700

    adding certificate

--- index.js
+++ index.js
@@ -31,4 +31,19 @@ var swiper = new Swiper('.mySwiper', {
     },
   });
 
+  var swiper2 = new Swiper(".s2", {
+    loop: true,
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    }
+  });
+
 
